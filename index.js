2c2716bf24e58f2a16ef37eca5c634e4a345b52a
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-11-26 16:19:24 -0600

    added paragraphs, length and copy to clipboard functionality.

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const $generatedContent = $(".generatedContent");
 
-let $arguments = $(".form-check-input");
-
-console.log($arguments);
 const $button = $("#generateContentBtn");
 
 $button.click(function () {
-  url = "https://loripsum.net/api/";
+  let $arguments = $(".form-check-input");
+  let $typeNumber = $("#typeNumber").val();
+  let $textLength = $("#length").val();
+  let url = "https://loripsum.net/api/";
+
+  if ($typeNumber.length !== 0) {
+    url = url.concat(`/${$typeNumber}`);
+  }
+
+  url = url.concat(`/${$textLength}`);
+
   for ($checkmark of $arguments) {
     if ($checkmark.checked === true) {
       console.log($checkmark.checked);
       url = url.concat(`/${$checkmark.id}`);
     }
   }
-  console.log(url);
+
   $.get(url, function (data) {
     $generatedContent.empty();
     $generatedContent.prepend(data);
   });
+});
+
+let $copy = $("#copy");
+$copy.click(function () {
+  navigator.clipboard.writeText($generatedContent.html());
 });
