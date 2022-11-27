4ec49df2e956ba01015c121eb0f9c6c427ce117f
JeremyEffinger <Jeremy.Effinger@gmail.com>
2022-11-27 16:09:10 -0600

    CORS added

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const $generatedContent = $(".generatedContent");
 const $button = $("#generateContentBtn");
 
 $button.click(function () {
-  let $arguments = $(".form-check-input");
-  let $typeNumber = $("#typeNumber").val();
-  let $textLength = $("#length").val();
-  let url = "https://loripsum.net/api/";
+  let $arguments = $(".form-check-input"); //html to generate
+  let $typeNumber = $("#typeNumber").val(); //number of paragraphs
+  let $textLength = $("#length").val(); //length of paragraphs
+  let url = "https://loripsum.net/api/"; //api url
 
   if ($typeNumber.length !== 0) {
     url = url.concat(`/${$typeNumber}`);
-  }
+  } //append number of paragraphs to get request
 
-  url = url.concat(`/${$textLength}`);
+  url = url.concat(`/${$textLength}`); //apppend length of paragraphs
 
+  //loop over options and append to get request
   for ($checkmark of $arguments) {
     if ($checkmark.checked === true) {
       console.log($checkmark.checked);
@@ -21,13 +22,14 @@ $button.click(function () {
     }
   }
 
+  //make get request to the constructed URL
   $.get(url, function (data) {
-    $generatedContent.empty();
-    $generatedContent.prepend(data);
+    $generatedContent.empty(); //destroy old data
+    $generatedContent.prepend(data); //prepend new data to html
   });
 });
 
 let $copy = $("#copy");
 $copy.click(function () {
-  navigator.clipboard.writeText($generatedContent.html());
+  navigator.clipboard.writeText($generatedContent.html()); //copy html in $generatedContent to clipboard.
 });
